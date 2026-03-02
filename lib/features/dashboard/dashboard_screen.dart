58ed10b56d339b0ddd192562409d5e4e6ef77d20
Nisith Nimtharu Welivitiya <155635480+NisithNim2007@users.noreply.github.com>
2026-03-02 12:35:22 +0530

    Revert "Merge pull request #15 from CGP-Group-31/sithija"
    This reverts commit e62f2c85aae72358df5798fcacd96646dbcae098, reversing
    changes made to 329844a9babad068a4f27356ea4fcf3db242e701.

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -1,125 +1,174 @@
 import 'package:flutter/material.dart';
-import '../auth/theme.dart';
+import '../../core/session/session_manager.dart';
+import '../auth/login_screen.dart';
+import '../dashboard/app_colors.dart';
+import '../dashboard/settings_screen.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
-  Widget buildCard({
-    required String title,
-    required String subtitle,
-    required IconData icon,
-    required Color iconColor,
-  }) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.only(bottom: 16),
-      decoration: BoxDecoration(
-        color: AppColors.containerBackground,
-        borderRadius: BorderRadius.circular(18),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 8,
-            offset: const Offset(0, 4),
-          )
-        ],
-      ),
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 26,
-            backgroundColor: iconColor.withOpacity(0.15),
-            child: Icon(icon, color: iconColor),
-          ),
-          const SizedBox(width: 16),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.primaryText,
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 4),
-              Text(
-                subtitle,
-                style: const TextStyle(
-                  color: AppColors.descriptionText,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+  Future<void> _logout(BuildContext context) async {
+    await SessionManager.logout();
+
+    if (!context.mounted) return;
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginScreen()),
+          (_) => false,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.mainBackground,
-      appBar: AppBar(
-        backgroundColor: AppColors.primary,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          "Caregiver Dashboard",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
 
-            buildCard(
-              title: "Missed Medicine",
-              subtitle: "Morning dose not taken",
-              icon: Icons.medication,
-              iconColor: AppColors.sosButton,
-            ),
-
-            buildCard(
-              title: "Hydration Alert",
-              subtitle: "Water intake is low",
-              icon: Icons.water_drop,
-              iconColor: AppColors.primary,
-            ),
-
-            buildCard(
-              title: "AI Check-in",
-              subtitle: "Last check 30 mins ago",
-              icon: Icons.health_and_safety,
-              iconColor: Colors.green,
-            ),
-
-            const Spacer(),
-
-            // SOS Section
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(18),
-              decoration: BoxDecoration(
-                color: AppColors.emergencyBackground,
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: const Center(
-                child: Text(
-                  "SOS TRIGGERED",
-                  style: TextStyle(
-                    color: AppColors.sosButton,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+              ///Header
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Home",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textPrimary,
+                    ),
                   ),
+                  IconButton(
+                    icon: const Icon(Icons.settings),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const SettingsScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 10),
+
+              const Text(
+                "Welcome, Sarah 👋",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: AppColors.textSecondary,
                 ),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 20),
+
+              ///AI Summary check in part
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: AppColors.containerBg,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Last AI Check in Summary",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textPrimary,
+                      ),
+                    ),
+
+                    SizedBox(height: 10),
+                    Text("* John was last active at the living room."),
+                    Text("* Hydration level lower than that of the usual."),
+                    Text("* Missed morning medicine at 8:00 AM"),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              ///Quick alerts
+              const Text(
+                "Quick Alerts",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.textPrimary,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              _alertTile(
+                title: "Missed Medicine",
+                subtitle: "Today, 8:30AM",
+                icon: Icons.medication,
+                color: AppColors.warning,
+              ),
+
+              _alertTile(
+                title: "Missed Check In",
+                subtitle: "Tuesday, 5:15PM",
+                icon: Icons.access_time,
+                color: AppColors.warning,
+              ),
+
+              _alertTile(
+                title: "Low Hydration",
+                subtitle: "Hydration level lower than usual",
+                icon: Icons.water_drop,
+                color: AppColors.warning,
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _alertTile({
+    required String title,
+    required String subtitle,
+    required IconData icon,
+    required Color color,
+  }) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 12),
+      padding: const EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: AppColors.containerBg,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Icon(icon, color: color,),
+          const SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textPrimary)),
+                const SizedBox(height: 4),
+                Text(
+                  subtitle,
+                  style: const TextStyle(color: AppColors.textSecondary),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
