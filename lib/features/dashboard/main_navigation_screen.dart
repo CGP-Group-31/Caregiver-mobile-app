c5e2d37df4e3f275b38b5ec16b616b9f81a14106
Cypher <156312972+Sithijaimesh@users.noreply.github.com>
2026-03-09 13:32:45 +0530

    Revert "Sithi"

--- lib/features/dashboard/main_navigation_screen.dart
+++ lib/features/dashboard/main_navigation_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import '../auth/elder_profile.dart';
 import '../dashboard/dashboard_screen.dart';
-import '../schedule/main_schedule.dart';
+import '../more/more_screen.dart';
+import '../schedule/schedule_screen.dart';
 import '../messages/messages_screen.dart';
 import '../alerts/alerts_screen.dart';
 
@@ -13,7 +14,6 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
-
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
@@ -22,44 +22,35 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context){
-
-    final screens = <Widget>[
+    final screens = <Widget> [
       const DashboardScreen(),
-      const MainScheduleScreen(),   // ✅ Schedule page
+      const ScheduleScreen(),
       const MessagesScreen(),
       ElderProfileScreen(onBackToHome: () => _onItemTapped(0)),
     ];
 
     return Scaffold(
       body: screens[_selectedIndex],
-
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-
         selectedItemColor: const Color(0xFF2E7D7A),
         unselectedItemColor: const Color(0xFF6F7F7D),
         backgroundColor: const Color(0xFFF6F7F3),
-
         type: BottomNavigationBarType.fixed,
-
         items: const [
-
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: "Home",
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.calendar_today),
             label: "Schedule",
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.message),
             label: "Messages",
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: "Elder Profile",
